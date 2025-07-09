c5914cc51a6648973d246faf298b40144ba24a91
Ch Sahasra <160098858+sahasra-10@users.noreply.github.com>
2025-07-09 11:57:38 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import joblib
 
+# Load trained model
+model = joblib.load('model/iris_model.pkl')
+
+# Example input (sepal length, sepal width, petal length, petal width)
+sample = pd.DataFrame({
+    'sepal_length': [5.1],
+    'sepal_width': [3.5],
+    'petal_length': [1.4],
+    'petal_width': [0.2]
+})
+
+# Predict species
+prediction = model.predict(sample)[0]
+print(f" Predicted Species: {prediction}")
